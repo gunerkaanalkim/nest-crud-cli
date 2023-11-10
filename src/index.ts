673f2c1a7367507ffe2335b7522d3e4621bf140d
gunerkaanalkim <g.kaanalkim@gmail.com>
2023-11-10 14:59:29 +0000

    execution options have been added

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,5 @@
 #!/usr/bin/env node
-import {input} from "@inquirer/prompts";
+import {input, select} from "@inquirer/prompts";
 import EntityCommand from "./commands/entity.command";
 import ControllerCommand from "./commands/controller.command";
 import DtoCommand from "./commands/dto.command";
@@ -9,86 +9,108 @@ import MapperCommand from "./commands/mapper.command";
 import chalk from "chalk";
 
 async function commandRunner() {
-    const className = await input({message: "Enter your entity name : "});
-    console.log(chalk.yellow(`Working directory is ${process.cwd()}`));
+    const answer = await select({
+        message: 'Please choose an option',
+        choices: [
+            {
+                name: 'generate',
+                value: 'generate',
+                description: 'generate everything that a nestjs module needs',
+            },
+            {
+                name: 'version',
+                value: 'version',
+                description: 'see nccli version',
+            }
+        ],
+    });
 
-    new EntityCommand().builder({
-        data: {
-            className: className,
-            entityName: `${className.toLowerCase()}s`
-        },
-        templatePath: "../../templates/entity.template.hbs",
-    }).execute();
+    if ('generate' === answer) {
+        const className = await input({message: "Enter your entity name : "});
+        console.log(chalk.yellow(`Working directory is ${process.cwd()}`));
 
-    new ControllerCommand().builder({
-        data: {
-            className: className,
-            entityName: className,
-            entityFileName: `./${className.toLowerCase()}.entity`,
-            dtoName: `${className}DTO`,
-            dtoFileName: `./${className.toLowerCase()}.dto`,
-            mapperName: `${className}Mapper`,
-            mapperVariableName: `${className.toLowerCase()}Mapper`,
-            mapperFileName: `./${className.toLowerCase()}.mapper`,
-            serviceName: `${className}Service`,
-            serviceVariableName: `${className.toLowerCase()}Service`,
-            serviceFileName: `./${className.toLowerCase()}.service`,
-            controllerPathName: `${className.toLowerCase()}s`
-        },
-        templatePath: "../../templates/controller.template.hbs",
-    }).execute();
+        new EntityCommand().builder({
+            data: {
+                className: className,
+                entityName: `${className.toLowerCase()}s`
+            },
+            templatePath: "../../templates/entity.template.hbs",
+        }).execute();
 
-    new DtoCommand().builder({
-        data: {
-            className: className
-        },
-        templatePath: "../../templates/dto.template.hbs",
-    }).execute();
+        new ControllerCommand().builder({
+            data: {
+                className: className,
+                entityName: className,
+                entityFileName: `./${className.toLowerCase()}.entity`,
+                dtoName: `${className}DTO`,
+                dtoFileName: `./${className.toLowerCase()}.dto`,
+                mapperName: `${className}Mapper`,
+                mapperVariableName: `${className.toLowerCase()}Mapper`,
+                mapperFileName: `./${className.toLowerCase()}.mapper`,
+                serviceName: `${className}Service`,
+                serviceVariableName: `${className.toLowerCase()}Service`,
+                serviceFileName: `./${className.toLowerCase()}.service`,
+                controllerPathName: `${className.toLowerCase()}s`
+            },
+            templatePath: "../../templates/controller.template.hbs",
+        }).execute();
 
-    new ServiceCommand().builder({
-        data: {
-            className: className,
-            entityName: className,
-            entityFileName: `./${className.toLowerCase()}.entity`,
-            dtoName: `${className}DTO`,
-            dtoFileName: `./${className.toLowerCase()}.dto`,
-            mapperName: `${className}Mapper`,
-            mapperVariableName: `${className.toLowerCase()}Mapper`,
-            mapperFileName: `./${className.toLowerCase()}.mapper`,
-            serviceName: `${className}Service`,
-            repositoryName: `${className.toLowerCase()}Repository`
-        },
-        templatePath: "../../templates/service.template.hbs",
-    }).execute();
+        new DtoCommand().builder({
+            data: {
+                className: className
+            },
+            templatePath: "../../templates/dto.template.hbs",
+        }).execute();
 
-    new ModuleCommand().builder({
-        data: {
-            className: className,
-            entityName: className,
-            entityFileName: `./${className.toLowerCase()}.entity`,
-            mapperName: `${className}Mapper`,
-            mapperFileName: `./${className.toLowerCase()}.mapper`,
-            serviceName: `${className}Service`,
-            serviceFileName: `./${className.toLowerCase()}.service`,
-            controllerName: `${className.charAt(0).toUpperCase() + className.slice(1)}Controller`,
-            controllerFileName: `./${className.toLowerCase()}.controller`,
-            moduleName: `${className.charAt(0).toUpperCase() + className.slice(1)}Module`,
-        },
-        templatePath: "../../templates/module.template.hbs",
-    }).execute();
+        new ServiceCommand().builder({
+            data: {
+                className: className,
+                entityName: className,
+                entityFileName: `./${className.toLowerCase()}.entity`,
+                dtoName: `${className}DTO`,
+                dtoFileName: `./${className.toLowerCase()}.dto`,
+                mapperName: `${className}Mapper`,
+                mapperVariableName: `${className.toLowerCase()}Mapper`,
+                mapperFileName: `./${className.toLowerCase()}.mapper`,
+                serviceName: `${className}Service`,
+                repositoryName: `${className.toLowerCase()}Repository`
+            },
+            templatePath: "../../templates/service.template.hbs",
+        }).execute();
 
-    new MapperCommand().builder({
-        data: {
-            className: className,
-            entityName: className,
-            entityFileName: `./${className.toLowerCase()}.entity`,
-            dtoName: `${className}DTO`,
-            dtoFileName: `./${className.toLowerCase()}.dto`,
-            mapperName: `${className}Mapper`
-        },
-        templatePath: "../../templates/mapper.template.hbs",
-    }).execute();
+        new ModuleCommand().builder({
+            data: {
+                className: className,
+                entityName: className,
+                entityFileName: `./${className.toLowerCase()}.entity`,
+                mapperName: `${className}Mapper`,
+                mapperFileName: `./${className.toLowerCase()}.mapper`,
+                serviceName: `${className}Service`,
+                serviceFileName: `./${className.toLowerCase()}.service`,
+                controllerName: `${className.charAt(0).toUpperCase() + className.slice(1)}Controller`,
+                controllerFileName: `./${className.toLowerCase()}.controller`,
+                moduleName: `${className.charAt(0).toUpperCase() + className.slice(1)}Module`,
+            },
+            templatePath: "../../templates/module.template.hbs",
+        }).execute();
 
+        new MapperCommand().builder({
+            data: {
+                className: className,
+                entityName: className,
+                entityFileName: `./${className.toLowerCase()}.entity`,
+                dtoName: `${className}DTO`,
+                dtoFileName: `./${className.toLowerCase()}.dto`,
+                mapperName: `${className}Mapper`
+            },
+            templatePath: "../../templates/mapper.template.hbs",
+        }).execute();
+    } else if ('help' === answer) {
+        console.log('help')
+
+    } else {
+        console.log(chalk.yellow(`nccli version is ${process.env.npm_package_version}`));
+    }
 }
 
 commandRunner();
